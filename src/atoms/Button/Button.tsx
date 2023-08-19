476bfcf68ada71a2aea8e81db82eb717d211b9c9
Shim su yeon <suyeonshim@gm.gist.ac.kr>
2023-08-20 05:12:15 +0900

    click button

--- src/atoms/Button/Button.tsx
+++ src/atoms/Button/Button.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import colorSet from "src/styles/colorSet";
 import styled from "styled-components";
 
@@ -9,7 +10,7 @@ interface ButtonProps {
   disabled?: boolean;
 }
 
-const Button = styled.button<ButtonProps>`
+const StyledButton = styled.button<ButtonProps>`
   width: ${(props) => props.width || "150px"};
   height: ${(props) => props.height || "150px"};
   border: ${(props) => props.border || "none"};
@@ -21,7 +22,9 @@ const Button = styled.button<ButtonProps>`
   font-weight: 500;
   margin: 15px;
   cursor: ${(props) => (props.disabled ? "not-allowed" : "pointer")};
+  transition: background-color 0.3s;
   &:hover {
+    border: 3px ${colorSet.primary};
     background-color: ${(props) =>
       props.disabled ? "#e0e0e0" : colorSet.gray40};
   }
@@ -29,5 +32,23 @@ const Button = styled.button<ButtonProps>`
     outline: none;
   }
 `;
+
+const Button = ({ ...props }) => {
+  const [clicked, setClicked] = useState(false);
+
+  const handleClick = () => {
+    setClicked(!clicked);
+  };
+
+  return (
+    <StyledButton
+      onClick={handleClick}
+      disabled={clicked} // Disabling the button when clicked
+      {...props}
+    >
+      {clicked ? "Clicked" : "Click Me"}
+    </StyledButton>
+  );
+};
 
 export default Button;
