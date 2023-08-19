b8377dd15f772e41a288aca47a433150f5a1c3a8
Jeongwoo Lee <crowntheking@gm.gist.ac.kr>
2023-08-19 23:03:23 +0900

    :sparkles: Added Table

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { Route, Routes } from "react-router-dom";
 
 import Layout from "./layout/Layout";
 import Homepage from "./pages/homepage/homepage";
+import Playground from "./pages/playground/Playground";
 import GlobalStyle from "./styles/globalStyles";
 
 const App = () => {
@@ -13,6 +14,7 @@ const App = () => {
       <Routes>
         <Route element={<Layout />}>
           <Route path="/" element={<Homepage />} />
+          <Route path="/pl" element={<Playground />} />
         </Route>
       </Routes>
     </>
--- src/styles/colorSet.ts
+++ src/styles/colorSet.ts
@@ -1,13 +1,19 @@
 const colorSet = {
-  primary: "#EB6263",
-  secondary: "#FFEAEA", // primary보다 연함
+  primary: "#00E27B",
 
-  // 흑백 계열 Colors
-  text: "#252525", // CSS 기본 "black"과의 혼동을 피하기 위해 Figma의 "black"을 "text"로 표기
-  secondaryText: "#959595",
-  deselected: "#D6D6D6",
-  placeholder: "#E3E3E3",
-  colorless: "#FFFFFF", // CSS 기본 "white"과의 혼동을 피하기 위해 "colorless"로 표기
+  black: "#000",
+  white: "#fff",
+  gray10: "#F5F5F5",
+  gray20: "#EBECED",
+  gray30: "#D9DADD",
+  gray40: "#969BA3",
+  gray50: "#393939",
+  gray35: "#C5C6C9",
+  gray05: "#FAFAFA",
+
+  disease: "#FF288F",
+  medication: "#FF4228",
+  bloodPressure: "#00B2FF",
 };
 
 export default colorSet;
